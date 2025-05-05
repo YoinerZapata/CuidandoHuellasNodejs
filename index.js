f6a1f3d6062d5b307c16d855f24383f333521702
Yoiner zapata <yoinerzapata23@MacBook-Pro-de-Yoiner.local>
2025-05-05 15:56:24 -0500

    Se renderizo la pagina_principal

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const exp = require("express")
+const path = require('path');
 const modeloProducto = require('./src/models/producto.model')
 
 const app = exp();
 
+// Motor de vistas
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(exp.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    // Si tienes lógica de sesión, pásala aquí:
+    //   const loggedIn = Boolean(req.session && req.session.pista);
+    //   res.render('pagina_principal', { loggedIn });
+    res.render('pagina_principal', { loggedIn: false });
+  });
+  
 app.get('/productos', async(req, res)=>{
   let listaProducto = await modeloProducto.find({});
   console.log(listaProducto)
@@ -13,6 +27,8 @@ app.get('/productos', async(req, res)=>{
   }
 
 })
+
+
 
 //callback 
 app.listen(7171,()=>{
